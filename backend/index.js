9e1c54cc2c86b32d8a732f2fd37051b9e578425f
Rijo Sebastian <rijobdk@gmail.com>
2023-09-26 12:40:46 +0530

    Finished basic CRUD opertion using mongo DB

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,27 @@
-import express from "express";
+import express, { request } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+//  Middleware for books routes
+app.use("books", booksRoute);
+
+// Middleware for CORS
+app.use(cors());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Hello World");
 });
 
-// mongodb
+// MongoDB connection and server start
 mongoose
   .connect(mongoDBURL)
   .then(() => {
