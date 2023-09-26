85ee410f749232ac706d63cb1b2ff8d5520e674a
Rijo Sebastian <rijobdk@gmail.com>
2023-09-26 10:01:17 +0530

    Connected to mongodb

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL= 'mongodb+srv://rijo:WF0D62sr9Y0iqDaq@books-store-mern-stack.qhxhxjz.mongodb.net/books-collection?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Hello World");
+});
+
+// mongodb
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("MongoDB successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
